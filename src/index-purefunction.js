62be7625b0b4b87b9220ecb3e131ebfdf3db568f
Subramanian Murugan <sasubramanian_md@hotmail.com>
2020-06-15 12:01:02 +0530

    first commit

---
+++ src/index-purefunction.js
@@ -0,0 +1,64 @@
+//pure function
+
+/**
+   Pure function is one the functional programming principle
+
+  Rules to certify the function as pure;
+
+   1.function which recives input, should return the same.
+    function doSomething(input){
+      return input
+    }
+   in react, all components are pure with respect to props.
+
+   2.what if the function needs to modifiy input
+
+   ---According to functional programming, state(Data) memory should not be
+   overriden,rather it should create/copy new memroy address
+      ->Immuability
+
+    let say its memory address is 1024
+     x = {id;1} ==> change
+ 
+ */
+
+ //how to write pure function with respects to state(Data) mutation
+
+ //impure function
+//  function updateCity(profile,city){
+//     profile.city=city;
+//     return profile;
+//  }
+
+//pure function : apply immutablity : dont override existing memory address
+ function updateCity(profile,city){
+   
+   //way : 1 : return new Object  ,by coping old object properties, supply updated prop
+    // return {
+    //   id: profile.id,
+    //   name:profile.name,
+    //   city:city
+    // }
+
+    //way 2: using javascript api 
+    //return Object.assign({},profile,{city:city});
+
+     //way 3 : Using es 7 spread Operator
+    // return {...profile,city:city}
+     return {...profile,city}
+ }
+
+
+ let profile ={id:1,name:'Subramanian',city:'Chennai'};
+
+ //testing api
+  Object.freeze(profile);
+
+ const updatedProfile = updateCity(profile,'Delhi');
+ console.log(updatedProfile)
+
+ /////////////////////////////////////////////////////////////////
+
+//Task :
+ let list = [1];
+ list.push() //pure or impure
--- src/index.js
+++ src/index.js
@@ -1,58 +1,125 @@
-//pure function
+import {render} from 'react-dom';
+import React ,{useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
 
-/**
-   Pure function is one the functional programming principle
+// class CounterComponent extends React.Component {
 
-  Rules to certify the function as pure;
+//     state = {
+//         counter: 0 // inital value
+//     };
 
-   1.function which recives input, should return the same.
-    function doSomething(input){
-      return input
-    }
-   in react, all components are pure with respect to props.
+//     //Listener method
+//     onIncrement = () => {
+//         let counter = this.state.counter + 1;
+//         this.setState({ counter });
+//     }
 
-   2.what if the function needs to modifiy input
+//     render() {
+//         console.log('render method is called', this.state.counter);
+//         return <div className="container">
+//             <h1>Counter Application</h1>
+//             <h3>Counter : {this.state.counter}</h3>
+//             <button className="btn btn-success" onClick={this.onIncrement}>+</button>
+//         </div>
+//     }
+// }
 
-   ---According to functional programming, state(Data) memory should not be
-   overriden,rather it should create/copy new memroy address
-      ->Immuability
+//basic useState Hook
+// function CounterComponent(props) {
+//     //hooks
+//     const [counter, setCounter] = useState(10)
 
-    let say its memory address is 1024
-     x = {id;1} ==> change
- 
- */
+//     function onDecrement() {
+//         setCounter(counter - 1)
+//     }
+//     return <div className="container">
+//         <h1>Counter Application</h1>
+//         <h2>Counter : {counter}</h2>
+//         {/* <button onClick={() => setCounter(prevState => {
+//             console.log(`Previouse State ${prevState}`)
+//             return prevState + 1
+//         })}> +</button> */}
+//         <button onClick={() => setCounter(counter + 1)}>+</button>
+//         <button onClick={onDecrement}>-</button>
 
- //how to write pure function with respects to state(Data) mutation
+//     </div>
+// }
+// const Application = () => <CounterComponent />
 
- //impure function
-//  function updateCity(profile,city){
-//     profile.city=city;
-//     return profile;
-//  }
-
-//pure function : apply immutablity : dont override existing memory address
- function updateCity(profile,city){
-   
-   //way : 1 : return new Object  ,by coping old object properties, supply updated prop
-    // return {
-    //   id: profile.id,
-    //   name:profile.name,
-    //   city:city
-    // }
-
-    //way 2: using javascript api 
-    //return Object.assign({},profile,{city:city});
-
-     //way 3 : Using es 7 spread Operator
-    // return {...profile,city:city}
-     return {...profile,city}
- }
+// ReactDOM.render(<Application />, document.getElementById('root'));
 
 
- let profile ={id:1,name:'Subramanian',city:'Chennai'};
+// const CounterComponent = (props) => {
+//     //hooks
+//     const { seedValue } = props;
+//     const [counter, setCounter] = useState(seedValue)
 
- //testing api
-  Object.freeze(profile);
+//     const onDecrement = () => {
+//         setCounter(counter - 1)
+//     }
+//     return <div className="container">
+//         <h1>Counter Application</h1>
+//         <h2>Counter : {counter}</h2>
+//         {/* <button onClick={() => setCounter(prevState => {
+//             console.log(`Previouse State ${prevState}`)
+//             return prevState + 1
+//         })}> +</button> */}
+//         <button onClick={() => setCounter(counter + 1)}>+</button>
+//         <button onClick={onDecrement}>-</button>
 
- const updatedProfile = updateCity(profile,'Delhi');
- console.log(updatedProfile)
+//     </div>
+// }
+
+// CounterComponent.defaultProps = {
+//     seedValue: 2
+// }
+
+// const Application = () => <>
+//     <CounterComponent seedValue={10} />
+//     <hr></hr>
+//     <CounterComponent />
+
+// </>
+
+// ReactDOM.render(<Application />, document.getElementById('root'));
+
+//multi state variable.
+
+
+// function Counter(props) {
+//       const [incrementValue, setincrementValue] = useState(props.incrementSeed);
+//       const [decrementValue, setdecrementValue] = useState(props.decrementSeed);
+      
+//       return <div>
+//             <h1>Counter App</h1>
+//             <h2>Increment Value : {incrementValue}</h2>
+//             <h2>Decrement Value : {decrementValue}</h2>
+
+//             <button onClick={() => setincrementValue(prevState => {
+//                   console.log(`Previouse State ${prevState}`)
+//                   return prevState + 1
+//             })}>
+//                   increment
+//           </button>
+//             <button onClick={() => setdecrementValue(decrementValue - 1)}>decrement</button>
+//       </div>
+// }
+
+
+// render(<Counter incrementSeed={50} decrementSeed={20} />, document.getElementById('root'));
+
+const FeedBack = (props) => {
+      const state = { like: 10 };
+      const [feedback, setFeedback] = useState(state);
+      const increment = () => {
+            let like = feedback.like + 1;
+            setFeedback({ ...feedback, like });
+      };
+      return <div>
+            <p>Your Feedback {feedback.like}</p>
+            <button onClick={increment}>
+                  increment
+         </button>
+      </div>;
+};
+render(<FeedBack />, document.getElementById('root'));
